7eb0eb3048e13695fafa923965efd2858f494c2b
Wong Wei Jun <wong.weijun923@gmail.com>
2026-05-23 10:12:23 +0800

    Added Route

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, private, users, utils
+from app.api.routes import items, login, private, users, utils, file, chat
 from app.core.config import settings
-from app.api.routes import chat
 
 api_router = APIRouter()
 api_router.include_router(login.router)
 api_router.include_router(users.router)
 api_router.include_router(utils.router)
 api_router.include_router(items.router)
-api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
+# api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
+api_router.include_router(file.router)
 
 if settings.ENVIRONMENT == "local":
     api_router.include_router(private.router)
